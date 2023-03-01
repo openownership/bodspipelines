aea61a1c26ea2ad0598c15b49123b977f07f68e2
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-03-01 08:45:26 +0000

    bodspipelines/infrastructure/outputs: Print item

--- bodspipelines/infrastructure/outputs.py
+++ bodspipelines/infrastructure/outputs.py
@@ -32,6 +32,7 @@ class NewOutput:
         self.output = output
 
     def process(self, item, item_type):
+        print(f"{item_type}: {item}")
         item = self.storage.process(item, item_type)
         if item:
             self.storage.process(item, item_type)
