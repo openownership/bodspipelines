3cf711baf3f34ad6b0f062721d09b6ab91822148
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-03-01 13:53:24 +0000

    bodspipelines/infrastructure: Add KinesisInput and JSONData

---
+++ bodspipelines/infrastructure/inputs.py
@@ -0,0 +1,12 @@
+from bodspipelines.infrastructure.clients.kinesis_client import KinesisStream
+
+class KinesisInput:
+    """Read from Kinesis Stream"""
+    def __init__(self, stream_name=None):
+        self.stream_name = stream_name
+        self.stream = KinesisStream(self.stream_name)
+
+    def process(self):
+        for records in self.stream.read_stream():
+            print("Records:", records)
+
---
+++ bodspipelines/infrastructure/processing/json_data.py
@@ -0,0 +1,5 @@
+class JSONData:
+    """JSON data definition class"""
+
+    def process(self, filename):
+        """Iterate over processed items from file"""
