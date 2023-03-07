d232d0a5be02bd7c5f24ea27b7c2851d9b8c76b9
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-03-07 13:36:47 +0000

    bodspipelines: Add transform pipeline stage

--- bodspipelines/mappings/gleif.py
+++ bodspipelines/mappings/gleif.py
@@ -122,3 +122,86 @@ def id_rr(item):
 
 def id_repex(item):
     return f"{item['LEI']}_{item['ExceptionCategory']}_{item['ExceptionReason']}"
+
+# BODS Entity Statement Elasticsearch Properties
+entity_statement_properties = {'statementID': {'type': 'text'},
+                               'statementType': {'type': 'text'},
+                               'statementDate': {'type': 'text'},
+                               'entityType': {'type': 'text'},
+                               'name': {'type': 'text'},
+                               'jurisdiction': {'type': 'text'},
+                               'identifiers': {'type': 'object',
+                                               'properties': {'id': {'type': 'text'},
+                                                              'scheme':  {'type': 'text'},
+                                                              'schemeName':  {'type': 'text'}}},
+                               'foundingDate': {'type': 'text'},
+                               'addresses': {'type': 'object',
+                                             'properties': {{'type': {'type': 'text'},
+                                                             'address': {'type': 'text'},
+                                                             'postCode': {'type': 'text'},
+                                                             'country': {'type': 'text'}}},
+                               'publicationDetails': {'type': 'object',
+                                                      'properties': {'publicationDate': {'type': 'text'},
+                                                                     'bodsVersion': {'type': 'text'},
+                                                                     'license': {'type': 'text'},
+                                                                     'publisher': {'type': 'object',
+                                                                                   'properties': {'name': {'type': 'text'},
+                                                                                                  'url': {'type': 'text'}}}},
+                               'source': {'type': 'object',
+                                          'properties': {'type': {'type': 'text'},
+                                                         'description': {'type': 'text'}}}}
+
+
+# BODS Entity Statement Elasticsearch Properties
+person_statement_properties = {'statementID': {'type': 'text'},
+                               'statementType': {'type': 'text'},
+                               'statementDate': {'type': 'text'},
+                               'personType': {'type': 'text'},
+                               'unspecifiedPersonDetails': {'type': 'object',
+                                                            'properties': {'reason': {'type': 'text'},
+                                                                           'description': {'type': 'text'}}}
+                               'source': {'type': 'object',
+                                          'properties': {'type': {'type': 'text'},
+                                                         'description': {'type': 'text'}}}}
+
+# BODS Ownership Or Control Statement 
+ownership_statement_properties = {'statementID': {'type': 'text'},
+                                  'subject': {'type': 'object',
+                                              'properties': {'describedByEntityStatement': {'type': 'text'}}},
+                                  'interestedParty': {'type': 'object',
+                                                      'properties': {'describedByEntityStatement': 'type': 'text'},
+                                                                     'describedByPersonStatement': 'type': 'text'}},
+                                  'interests': {'type': 'object',
+                                                'properties': {'type': {'type': 'text'},
+                                                               'interestLevel': {'type': 'text'},
+                                                               'beneficialOwnershipOrControl': ,
+                                                               'details': {'type': 'text'}}},
+                                  'publicationDetails': {'type': 'object',
+                                                         'properties': {'publicationDate': {'type': 'text'},
+                                                                        'bodsVersion': {'type': 'text'},
+                                                                        'license': {'type': 'text'},
+                                                                        'publisher': {'type': 'object',
+                                                                                   'properties': {'name': {'type': 'text'},
+                                                                                                  'url': {'type': 'text'}}}},
+                                  'source': {'type': 'object',
+                                          'properties': {'type': {'type': 'text'},
+                                                         'description': {'type': 'text'}}}}
+
+
+def match_entity(item):
+    return {"match": {"statementID": item["statementID"]}}
+
+def match_person(item):
+    return {"match": {"statementID": item["statementID"]}}
+
+def match_ownership(item):
+    return {"match": {"statementID": item["statementID"]}}
+
+def id_entity(item):
+    return item["statementID"]
+
+def id_person(item):
+    return item["statementID"]
+
+def id_ownership(item):
+    return item["statementID"]
--- bodspipelines/pipelines/gleif.py
+++ bodspipelines/pipelines/gleif.py
@@ -1,12 +1,16 @@
 from bodspipelines.infrastructure.pipeline import Source, Stage, Pipeline
+from bodspipelines/infrastructure/inputs import KinesisInput
 from bodspipelines.infrastructure.storage import ElasticStorage
 from bodspipelines.infrastructure.outputs import Output, OutputConsole, NewOutput, KinesisOutput
 from bodspipelines.infrastructure.processing.bulk_data import BulkData
 from bodspipelines.infrastructure.processing.xml_data import XMLData
 from bodspipelines.transforms.gleif import Gleif2Bods
-from bodspipelines.mappings.gleif import (lei_properties, rr_properties, repex_properties, 
+from bodspipelines.mappings.gleif import (lei_properties, rr_properties, repex_properties,
                                           match_lei, match_rr, match_repex,
                                           id_lei, id_rr, id_repex)
+from bodspipelines.mappings.gleif import (entity_statement_properties, person_statement_properties, ownership_statement_properties,
+                                          match_entity, match_person, match_ownership,
+                                          id_entity, id_person, id_ownership)
 
 # Defintion of LEI-CDF v3.1 XML date source
 lei2_source = Source(name="lei2",
@@ -51,16 +55,27 @@ ingest_stage = Stage(name="ingest",
               outputs=[output_new]
 )
 
+gleif_stream = KinesisInput(stream_arn="arn:aws:kinesis:eu-west-1:696709126511:stream/gleif-dev")
+
+bods_index_properties = {"entity": {"properties": entity_statement_properties, "match": match_entity, "id": id_entity},
+                         "person": {"properties": person_statement_properties, "match": match_person, "id": id_person},
+                         "ownership": {"properties": ownership_statement_properties, "match": match_ownership, "id": id_ownership}}
+
+bods_output_new = NewOutput(storage=ElasticStorage(indexes=bods_index_properties),
+                            output=KinesisOutput(stream_arn="arn:aws:kinesis:eu-west-1:696709126511:stream/gleif-dev"))
+
 # Definition of GLEIF data pipeline transform stage
-#transform_stage = Stage(name="transform",
-#              sources=[lei2_stream, rr_stream, repex_stream],
-#              processors=[Gleif2Bods()],
-#              outputs=[output_console]
-#)
+transform_stage = Stage(name="transform",
+              sources=[gleif_stream],
+              processors=[Gleif2Bods()],
+              outputs=[bods_output_new]
+)
 
 # Definition of GLEIF data pipeline
-pipeline = Pipeline(name="gleif", stages=[ingest_stage]) #, transform_stage])
+pipeline = Pipeline(name="gleif", stages=[ingest_stage, transform_stage])
 
-setup = ElasticStorage(indexes=index_properties).setup_indexes
+def setup():
+    ElasticStorage(indexes=index_properties).setup_indexes()
+    ElasticStorage(indexes=bods_index_properties).setup_indexes()
 
 stats = ElasticStorage(indexes=index_properties).stats
