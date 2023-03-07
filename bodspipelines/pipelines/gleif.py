e2bdbc545eb4eb93fe0edef6d134dda8e1d87331
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-03-07 21:56:15 +0000

    bodspipelines/pipelines/gleif: New test stream

--- bodspipelines/pipelines/gleif.py
+++ bodspipelines/pipelines/gleif.py
@@ -47,7 +47,7 @@ index_properties = {"lei2": {"properties": lei_properties, "match": match_lei, "
                     "repex": {"properties": repex_properties, "match": match_repex, "id": id_repex}}
 
 output_new = NewOutput(storage=ElasticStorage(indexes=index_properties),
-                       output=KinesisOutput(stream_arn="arn:aws:kinesis:eu-west-1:696709126511:stream/gleif-dev"))
+                       output=KinesisOutput(stream_arn="arn:aws:kinesis:eu-west-1:696709126511:stream/test-gleif-dev"))
 
 # Definition of GLEIF data pipeline injest stage
 ingest_stage = Stage(name="ingest",
@@ -57,7 +57,7 @@ ingest_stage = Stage(name="ingest",
 )
 
 gleif_source = Source(name="gleif",
-                      origin=KinesisInput(stream_arn="arn:aws:kinesis:eu-west-1:696709126511:stream/gleif-dev"),
+                      origin=KinesisInput(stream_arn="arn:aws:kinesis:eu-west-1:696709126511:stream/test-gleif-dev"),
                       datatype=JSONData())
 
 bods_index_properties = {"entity": {"properties": entity_statement_properties, "match": match_entity, "id": id_entity},
