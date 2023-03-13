3151817295f792732521f054742a5bd32def6ebf
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-03-13 13:45:33 +0000

    bodspipelines/pipelines/gleif: Change Kinesis streams

--- bodspipelines/pipelines/gleif.py
+++ bodspipelines/pipelines/gleif.py
@@ -56,7 +56,7 @@ index_properties = {"lei2": {"properties": lei_properties, "match": match_lei, "
 
 # GLEIF data: Store in Easticsearch and output new to Kinesis stream
 output_new = NewOutput(storage=ElasticStorage(indexes=index_properties),
-                       output=KinesisOutput(stream_name="gleif-prod-alt4"))
+                       output=KinesisOutput(stream_name="gleif-raw-dev")) #gleif-prod-alt4"))
 
 # Definition of GLEIF data pipeline ingest stage
 ingest_stage = Stage(name="ingest",
@@ -67,7 +67,7 @@ ingest_stage = Stage(name="ingest",
 
 # Kinesis stream of GLEIF data from ingest stage
 gleif_source = Source(name="gleif",
-                      origin=KinesisInput(stream_name="gleif-prod-alt4"),
+                      origin=KinesisInput(stream_name="gleif-raw-dev")) #gleif-prod-alt4"),
                       datatype=JSONData())
 
 # Elasticsearch indexes for BODS data
@@ -95,7 +95,7 @@ def identify_bods(item):
 
 # BODS data: Store in Easticsearch and output new to Kinesis stream
 bods_output_new = NewOutput(storage=ElasticStorage(indexes=bods_index_properties),
-                            output=KinesisOutput(stream_name="bods-gleif-prod-alt6"),
+                            output=KinesisOutput(stream_name="bods-gleif-dev")) #bods-gleif-prod-alt6"),
                             identify=identify_bods)
 
 # Definition of GLEIF data pipeline transform stage
