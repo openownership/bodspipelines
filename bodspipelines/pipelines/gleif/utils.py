270c23d1869af00ba09f22a7d35518eb5de62924
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-03-15 12:43:33 +0000

    bodspipelines/pipelines/gleif/utils: Add console output for get_source

--- bodspipelines/pipelines/gleif/utils.py
+++ bodspipelines/pipelines/gleif/utils.py
@@ -3,12 +3,13 @@ from bodspipelines.infrastructure.utils import download_delayed
 def get_source(r, name):
     data = r.json()
     if name == 'lei':
-        return data['data']['lei2']['full_file']['xml']['url']
+        url = data['data']['lei2']['full_file']['xml']['url']
     elif name == 'rr':
-        return data['data']['rr']['full_file']['xml']['url']
+        url = data['data']['rr']['full_file']['xml']['url']
     elif name == 'repex':
-        return data['data']['repex']['full_file']['xml']['url']
-    return None
+        url = data['data']['repex']['full_file']['xml']['url']
+    print(f"Using: {url}")
+    return url
 
 def gleif_download_link(url):
     return download_delayed(url, get_source)
