d4a37d427fdb963f1faef7123b842a50f77b5000
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-02-22 22:53:58 +0000

    bodspipelines/infrastructure/inputs.py: Fix Kinesis inputs

--- bodspipelines/infrastructure/inputs.py
+++ bodspipelines/infrastructure/inputs.py
@@ -1,21 +1,26 @@
-from bodspipelines.infrastructure.clients.kinesis_client import KinesisStream
+#from bodspipelines.infrastructure.clients.kinesis_client import KinesisStream
+from bodspipelines.infrastructure.clients.kinesis_client import create_kinesis
 
 class KinesisInput:
     """Read from Kinesis Stream"""
     def __init__(self, stream_name=None):
         self.stream_name = stream_name
-        self.stream = KinesisStream(self.stream_name)
+        #self.stream = KinesisStream(self.stream_name)
 
     async def process(self):
-        async for record in self.stream.read_stream():
+        #async for record in self.stream.read_stream():
             #for record in records:
             #    if isinstance(record, dict):
+        async for record in self.stream.get():
             yield record
 
     async def setup(self):
-        if hasattr(self.stream, 'setup'):
-            await self.stream.setup()
+        self.kinesis = create_kinesis(self.stream_name)
+        self.stream = await anext(self.kinesis)
+#        if hasattr(self.stream, 'setup'):
+#            await self.stream.setup()
 
     async def close(self):
-        if hasattr(self.stream, 'close'):
-            await self.stream.close()
+        #if hasattr(self.stream, 'close'):
+        #    await self.stream.close()
+        await anext(self.kinesis)
