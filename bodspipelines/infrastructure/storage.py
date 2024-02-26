21149e89e9ea8bcb7c790c60bd95cc2d9a83eec6
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-02-26 16:06:25 +0000

    bodspipelines/infrastructure/storage.py: Recreate indexes

--- bodspipelines/infrastructure/storage.py
+++ bodspipelines/infrastructure/storage.py
@@ -12,7 +12,7 @@ class ElasticStorage:
 
     def setup_indexes(self):
         for index_name in self.indexes:
-            if index_name in ("latest", "updates"): self.storage.create_index(index_name, self.indexes[index_name]['properties'])
+            if index_name in ("entity", "person"): self.storage.create_index(index_name, self.indexes[index_name]['properties'])
 
     def create_action(self, index_name, item):
         #print(index_name, item)
