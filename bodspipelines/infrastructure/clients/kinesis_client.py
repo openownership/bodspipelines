5710344c26551d6e8e10675b688450ae40c1686f
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-06-30 07:58:48 +0100

    kinesis_client.py: Improved client

--- bodspipelines/infrastructure/clients/kinesis_client.py
+++ bodspipelines/infrastructure/clients/kinesis_client.py
@@ -2,138 +2,48 @@ import os
 import time
 import json
 import gzip
-#import boto3
-from aiobotocore.session import get_session
 
-async def create_client(service):
-     """Create AWS client for specified service"""
-     session = get_session()
-     return await session.create_client(service, region_name=os.getenv('BODS_AWS_REGION'),
-                                  aws_access_key_id=os.environ.get('BODS_AWS_ACCESS_KEY_ID'),
-                                  aws_secret_access_key=os.environ.get('BODS_AWS_SECRET_ACCESS_KEY')).__aenter__()
-     #return boto3.client(service, region_name=os.getenv('BODS_AWS_REGION'), 
-     #                             aws_access_key_id=os.environ.get('BODS_AWS_ACCESS_KEY_ID'),
-     #                             aws_secret_access_key=os.environ.get('BODS_AWS_SECRET_ACCESS_KEY'))
-
-async def get_stream_arn(client, stream_name):
-    description = await client.describe_stream(StreamName=stream_name)
-    return description["StreamDescription"]["StreamARN"]
-
-async def shard_id(client, stream_arn):
-     """Generate shard id"""
-     response = await client.describe_stream(StreamARN=stream_arn)
-     return response['StreamDescription']['Shards'][0]['ShardId']
-
-def unpack_records(record_response):
-    """Unpack records"""
-    #print(record_response)
-    records = []
-    for record in record_response['Records']:
-        #records.append(json.loads(gzip.decompress(record['Data']).decode('utf-8')))
-        for line in record['Data'].decode('utf-8').split('\n'):
-            #print(line)
-            if line: records.append(json.loads(line))
-    return records
+from kinesis import Producer, Consumer, JsonLineProcessor
 
 class KinesisStream:
     """Kinesis Stream class"""
-    def __init__(self, stream_name=None, shard_count=1):
+    def __init__(self, stream_name=None):
         """Initial setup"""
-        self.client = None
+        self.producer = None
+        self.consumer = None
         self.stream_name = stream_name
-        self.stream_arn = None
-        self.shard_id = None
-        self.shard_count = shard_count
-        self.records = []
-        self.waiting_bytes = 0
 
     async def setup(self):
-        self.client = await create_client('kinesis')
-        self.stream_arn = await get_stream_arn(self.client, self.stream_name)
-        self.shard_id = await shard_id(self.client, self.stream_arn)
-
-    async def send_records(self):
-        """Send accumulated records"""
-        print(f"Sending {len(self.records)} records to {self.stream_arn}")
-        failed = len(self.records)
-        while failed == len(self.records):
-            response = await self.client.put_records(Records=self.records, StreamARN=self.stream_arn) #, StreamARN='string')
-            failed = response['FailedRecordCount']
-            if failed == len(self.records):
-                time.sleep(1)
-            elif failed > 0:
-                batch = self.records
-                self.records = []
-                self.waiting_bytes = 0
-                for i, record in enumerate(response['Records']):
-                    if 'ErrorCode' in record:
-                        self.records.append(batch[i])
-                        self.waiting_bytes += len(batch[i]["Data"])
-                break
-            else:
-                self.records = []
-                self.waiting_bytes = 0
-                break
+        """Setup Kinesis clients"""
+        self.producer = await Producer(stream_name=self.stream_name, processor=JsonLineProcessor(),
+                        region_name=os.getenv('BODS_AWS_REGION'),
+                        put_bandwidth_limit_per_shard=800).__aenter__()
+        self.consumer = await Consumer(stream_name=self.stream_name, processor=JsonLineProcessor(),
+                                 region_name=os.getenv('BODS_AWS_REGION')).__aenter__()
 
     async def add_record(self, record):
         """Add record to stream"""
-        json_data = json.dumps(record) + "\n"
-        #encoded_data = bytes(json_data, 'utf-8')
-        #compressed_data = gzip.compress(json_data.encode('utf-8'))
-        self.records.append({"Data": json_data, "PartitionKey": str(self.shard_count)})
-        num_bytes = len(json_data)
-        self.waiting_bytes += num_bytes
-        #print(f"Added {num_bytes} byte record ...")
-        #print(f"Batched records {len(self.records)}")
-        if self.waiting_bytes > 500000 or len(self.records) > 485: await self.send_records()
+        await self.producer.put(record)
 
     async def finish_write(self):
         """Write any remaining records"""
-        if len(self.records) > 0: await self.send_records()
+        await self.producer.flush()
 
     async def read_stream(self):
         """Read records from stream"""
-        shard_iterator = await self.client.get_shard_iterator(StreamARN=self.stream_arn,
-	                                                ShardId=self.shard_id,
-                                                        ShardIteratorType='TRIM_HORIZON')
-	                                                #ShardIteratorType='LATEST')
-        shard_iterator = shard_iterator['ShardIterator']
-        empty = 0
+        found = False
+        count = 0
         while True:
-            record_response = await self.client.get_records(ShardIterator=shard_iterator, Limit=100)
-            #print(record_response)
-            if len(record_response['Records']) == 0 and record_response['MillisBehindLatest'] == 0:
-                empty += 1
-            else:
-                if len(record_response['Records']) > 0:
-                    empty = 0
-                    for item in unpack_records(record_response):
-                        yield item
-            if empty > 250:
-                print(f"No records found in {self.stream_arn} after {empty} retries")
-                break
-            elif 'NextShardIterator' in record_response:
-                shard_iterator = record_response['NextShardIterator']
-            else:
-                break
+            async for item in self.consumer:
+                yield item
+                found = True
+                count = 0
+            print("Waiting for records...")
+            if found: count += 1
+            if found and count > 10: break
+        print("No more records")
 
-#    def read_stream(self):
-#        """Read records from stream"""
-#        stream = self.client.describe_stream(StreamName=self.stream_name)
-#        shard_id = stream["StreamDescription"]["Shards"][0]["ShardId"]
-#        print(f"Got {shard_id=}")
-#        iterator = self.client.get_shard_iterator(
-#            StreamName=self.stream_name,
-#            ShardId=shard_id,
-#            ShardIteratorType="TRIM_HORIZON"
-#            )["ShardIterator"]
-#        print(f"Reading data...")
-#        response = self.client.get_records(ShardIterator=iterator, Limit=1)
-#        while "NextShardIterator" in response:
-#            data = response["Records"]
-#            if len(data) < 1:
-#                print("No data received")
-#            else:
-#                data = data[0]["Data"]
-#                print(f"Received {data=}")
-#            response = self.client.get_records(ShardIterator=response["NextShardIterator"], Limit=1)
+    async def close(self):
+        """Close Kinesis clients"""
+        if self.producer: await self.producer.close()
+        if self.consumer: await self.consumer.close()
