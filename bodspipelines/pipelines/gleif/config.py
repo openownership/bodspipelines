07cfb79ec950356b526bb4ff422388fa44a790a7
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-07-02 14:42:11 +0100

    config.py: Re-enable lei processing

--- bodspipelines/pipelines/gleif/config.py
+++ bodspipelines/pipelines/gleif/config.py
@@ -113,7 +113,7 @@ output_new = NewOutput(storage=ElasticStorage(indexes=gleif_index_properties),
 
 # Definition of GLEIF data pipeline ingest stage
 ingest_stage = Stage(name="ingest",
-              sources=[ #lei_source,
+              sources=[lei_source,
                        rr_source,
                        repex_source],
               #sources=[repex_source],
