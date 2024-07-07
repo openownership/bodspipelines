e21d9a63935bf9c4d0fe99b2745f3413cd10df6e
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-07-07 15:21:01 +0100

    xml_data.py: Async file streaming

--- bodspipelines/infrastructure/processing/xml_data.py
+++ bodspipelines/infrastructure/processing/xml_data.py
@@ -1,4 +1,18 @@
-from lxml import etree
+import aiofiles
+#from lxml import etree
+import xml.etree.ElementTree as etree
+
+
+async def stream_file(filename):
+    async with aiofiles.open(filename, mode='r') as f:
+        while True:
+            data = await f.read(65536)
+            if data:
+                yield data
+            else:
+                yield ""
+                break
+
 
 def is_plural(tag, child_tag):
     """Is tag name plural"""
@@ -29,65 +43,75 @@ def get_tag(element, pos):
             return element.tag[pos[ns]:]
 
 
+def handle_event(event, element, tag_name, skip, stack, pos, filter):
+    out = None
+    tag = get_tag(element, pos)
+    if event == 'start':
+        if skip:
+            pass
+        elif tag in filter:
+            skip = True
+        elif element.tag == tag_name or stack:
+            stack.append([element.tag, {}])
+    elif event == 'end':
+        if skip:
+            if tag in filter:
+                skip = False
+        elif element.tag == tag_name:
+            element.clear()
+            # Also eliminate now-empty references from the root node to elem
+            #for ancestor in element.xpath('ancestor-or-self::*'):
+            #    while ancestor.getprevious() is not None:
+            #        del ancestor.getparent()[0]
+            elem = stack.pop()
+            out = elem[1]
+        elif stack:
+            elem = stack.pop()
+            if elem[1]:
+                val = elem[1]
+            else:
+                val = element.text
+            if stack[-1][1]:
+                if isinstance(stack[-1][1], list):
+                    if 'type' in element.attrib:
+                        if isinstance(val, dict):
+                            val['type'] = element.attrib['type']
+                            stack[-1][1].append(val)
+                        else:
+                            stack[-1][1].append({'type': element.attrib['type'], tag: val})
+                    else:
+                        stack[-1][1].append(val)
+                else:
+                    stack[-1][1][tag] = val
+            else:
+                if is_plural(stack[-1][0], tag):
+                    if 'type' in element.attrib:
+                        if isinstance(val, dict):
+                            val['type'] = element.attrib['type']
+                            stack[-1][1] = [val]
+                        else:
+                            stack[-1][1] = [{'type': element.attrib['type'], tag: val}]
+                    else:
+                        if isinstance(stack[-1][1], list):
+                            stack[-1][1].append(val)
+                        else:
+                            stack[-1][1] = [val]
+                else:
+                    stack[-1][1][tag] = val
+    return out, skip
+
+
 async def data_stream(filename, tag_name, namespaces, filter=[]):
     """Stream items from XML file"""
     skip = False
     stack = []
     pos = {namespaces[ns]: len(namespaces[ns])+2 for ns in namespaces}
-    for event, element in etree.iterparse(filename, events=('start', 'end',)):
-        tag = get_tag(element, pos)
-        if event == 'start':
-            if skip:
-                pass
-            elif tag in filter:
-                skip = True
-            elif element.tag == tag_name or stack:
-                stack.append([element.tag, {}])
-        elif event == 'end':
-            if skip:
-                if tag in filter:
-                    skip = False
-            elif element.tag == tag_name:
-                element.clear()
-                # Also eliminate now-empty references from the root node to elem
-                for ancestor in element.xpath('ancestor-or-self::*'):
-                    while ancestor.getprevious() is not None:
-                        del ancestor.getparent()[0]
-                elem = stack.pop()
-                yield elem[1]
-            elif stack:
-                elem = stack.pop()
-                if elem[1]:
-                    val = elem[1]
-                else:
-                    val = element.text
-                if stack[-1][1]:
-                    if isinstance(stack[-1][1], list):
-                        if 'type' in element.attrib:
-                            if isinstance(val, dict):
-                                val['type'] = element.attrib['type']
-                                stack[-1][1].append(val)
-                            else:
-                                stack[-1][1].append({'type': element.attrib['type'], tag: val})
-                        else:
-                            stack[-1][1].append(val)
-                    else:
-                        stack[-1][1][tag] = val
-                else:
-                    if is_plural(stack[-1][0], tag):
-                        if 'type' in element.attrib:
-                            if isinstance(val, dict):
-                                val['type'] = element.attrib['type']
-                                stack[-1][1] = [val]
-                            else:
-                                stack[-1][1] = [{'type': element.attrib['type'], tag: val}]
-                        else:
-                            if isinstance(stack[-1][1], list):
-                                stack[-1][1].append(val)
-                            else:
-                                stack[-1][1] = [val]
-                    else:
-                        stack[-1][1][tag] = val
+    parser = etree.XMLPullParser(('start', 'end',))
+    async for chunk in stream_file(filename):
+        parser.feed(chunk)
+        for event, element in parser.read_events():
+            out, skip = handle_event(event, element, tag_name, skip, stack, pos, filter)
+            if out: yield out
 
 
 class XMLData:
