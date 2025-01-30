492fffbb4e9963e3f5f24c7bed4fba4a4a759a36
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2025-01-30 16:30:25 +0000

    updates.py: Debug

--- bodspipelines/infrastructure/updates.py
+++ bodspipelines/infrastructure/updates.py
@@ -157,6 +157,7 @@ class ProcessUpdates:
                     statement["annotations"].extend(extra_annotations)
             if status:
                 await record_save(self.cache, statement["recordId"], statement_id, status, updates=updates)
+                print(statement)
                 yield statement
 
     async def finish_updates(self, updates=False):
